7372b8541ec0d7ed90e6c020085f455f0ec94564
苟波 <goubo297@pingan.com.cn>
2018-11-14 17:05:40 +0800

    删掉@Override

--- android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
+++ android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
@@ -1,7 +1,5 @@
 package com.reactnativecomponent.barcode;
 
-import android.app.Activity;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
@@ -14,16 +12,8 @@ import java.util.List;
 
 
 public class RCTCapturePackage implements ReactPackage {
-//    Activity activity;
     RCTCaptureModule mModuleInstance;
     RCTCaptureManager captureManager;
-//    RCTLinearGradientViewManager linearGradientViewManager;
-
-//   public RCTCapturePackage(Activity activity) {
-//            this.activity = activity;
-//        captureManager = new RCTCaptureManager(activity);
-////        linearGradientViewManager = new RCTLinearGradientViewManager(activity);
-//    }
 
     public RCTCapturePackage() {
         captureManager = new RCTCaptureManager();
@@ -31,24 +21,20 @@ public class RCTCapturePackage implements ReactPackage {
 
 
     @Override
-        public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
-             mModuleInstance = new RCTCaptureModule(reactApplicationContext,captureManager);
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+        mModuleInstance = new RCTCaptureModule(reactApplicationContext, captureManager);
         return Arrays.<NativeModule>asList(
                 mModuleInstance
         );
-        }
-
-        @Override
-        public List<Class<? extends JavaScriptModule>> createJSModules() {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
-            //noinspection ArraysAsListWithZeroOrOneArgument
-
-//            return Arrays.<ViewManager>asList(captureManager,linearGradientViewManager);
-            return Arrays.<ViewManager>asList(captureManager);
-        }
-
     }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        return Arrays.<ViewManager>asList(captureManager);
+    }
+
+}
